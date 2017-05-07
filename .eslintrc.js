35bf7e72a28a7d6683bed648a2f29c3843983b9a
Will Ockelmann.Wagner <will.ockelmann.wagner@gmail.com>
2017-05-06 17:59:02 -0700

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,15 @@ module.exports = {
     "extends": "airbnb-base",
     "plugins": [
         "import"
-    ]
+    ],
+    "rules": {
+      "strict": "off",
+      "no-console": "warn",
+      "arrow-parens": ["error", "as-needed"],
+      "comma-dangle": ["warn", "only-multiline"]
+    },
+    "globals": {
+      "chrome": true,
+      "WebSocket": true,
+    }
 };
